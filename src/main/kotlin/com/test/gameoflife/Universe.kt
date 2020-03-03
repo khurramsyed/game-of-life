3c3f7c3002bb680ed203e80b5784c65a84ba6d80
khurram <mahmood.khurram@gmail.com>
2020-03-03 20:36:31 +0000

    adding logic to evolve universe

--- src/main/kotlin/com/test/gameoflife/Universe.kt
+++ src/main/kotlin/com/test/gameoflife/Universe.kt
@@ -1,20 +1,80 @@
 package com.test.gameoflife
 
+
 class Universe(var size: Int) {
     var cells = Array(size) { Array(size) { Cell(false) } }
 
+    fun evolve() {
+        cells = generateNextState()
+    }
+
+    private fun generateNextState() : Array<Array<Cell>>{
+        var nextState = Array(size) { Array(size) { Cell(false) } }
+
+        for (row in 0 until size) {
+            for (column in 0 until size) {
+                nextState[row][column].isAlive = evolveCell(row, column)
+            }
+        }
+        return nextState
+    }
+
+    private fun evolveCell(row: Int, column: Int) =
+        cells[row][column].nextMove(getLiveNeighbourCount(Pair(row, column)))
+
+
     fun startUniverse(vararg seed: Pair<Int, Int>) {
         seed.forEach {
-            if (isValidIndex(it)) {
+            if (isInsideUniverse(it)) {
                 cells[it.first][it.second].isAlive = true
             }
         }
     }
 
-    private fun isValidIndex(coordinates: Pair<Int, Int>): Boolean {
+    private fun isInsideUniverse(coordinates: Pair<Int, Int>): Boolean {
         return (coordinates.first >= 0 && coordinates.first < cells.size) &&
                 (coordinates.second >= 0 && coordinates.second < cells.size)
     }
 
+    private fun getLiveNeighbourCount(cellLocation: Pair<Int, Int>): Int {
+        val neighbors = listOf(
+                Pair(cellLocation.first - 1, cellLocation.second - 1),
+                Pair(cellLocation.first, cellLocation.second - 1),
+                Pair(cellLocation.first + 1, cellLocation.second - 1),
+                Pair(cellLocation.first - 1, cellLocation.second),
+                Pair(cellLocation.first + 1, cellLocation.second),
+                Pair(cellLocation.first - 1, cellLocation.second + 1),
+                Pair(cellLocation.first, cellLocation.second + 1),
+                Pair(cellLocation.first + 1, cellLocation.second + 1)
+        ).filter { isInsideUniverse(it) && cells[it.first][it.second].isAlive }
+        return neighbors.size
+
+    }
+
+    private fun drawBoard() {
+        println("\u001b[H\u001b[2J")
+        System.out.flush();
+        for (row in 0 until size) {
+            for (column in 0 until size) {
+                print(if (cells[row][column].isAlive) "#" else ".")
+                print(" ")
+            }
+            println()
+        }
+    }
+
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            val universe = Universe(25)
+            universe.startUniverse(Pair(2, 2), Pair(3, 3), Pair(4, 1), Pair(4, 2), Pair(4, 3))
+
+            for (i in 0 until 50) {
+                universe.evolve()
+                universe.drawBoard();
+                Thread.sleep(300)
+            }
+        }
+    }
 
 }
--- src/test/kotlin/com/test/gameoflife/UniverseTest.kt
+++ src/test/kotlin/com/test/gameoflife/UniverseTest.kt
@@ -16,25 +16,93 @@ class UniverseTest {
     private fun assertAllCellsAreDead(universe: Universe) {
         universe.cells.forEach { row ->
             row.forEach { cell ->
-                assertThat(cell.isAlive).isEqualTo(false)
+                assertThat(cell.isAlive).isFalse()
             }
         }
     }
 
     @Test
-    fun universeIsStartedWithLiveCells() {
-        val universe = Universe(UNIVERS_SIZE)
-        universe.startUniverse(Pair(0, 1), Pair(2, 1))
-        assertThat(universe.cells[0][1].isAlive).isEqualTo(true)
-        assertThat(universe.cells[2][1].isAlive).isEqualTo(true)
+    fun universeShouldStayTheSameWithBeeHive() {
+        val universe = Universe(4)
+        universe.startUniverse(
+            Pair(0,1),Pair(0,2),
+            Pair(1,0),Pair(1,3),
+            Pair(2,1), Pair(2,2)
+        )
+
+        universe.evolve()
+        assertThat(universe.cells[0][1].isAlive).isTrue()
+        assertThat(universe.cells[0][2].isAlive).isTrue()
+        assertThat(universe.cells[1][0].isAlive).isTrue()
+        assertThat(universe.cells[1][3].isAlive).isTrue()
+        assertThat(universe.cells[2][1].isAlive).isTrue()
+        assertThat(universe.cells[2][2].isAlive).isTrue()
+
     }
 
     @Test
     fun universeIsStartedWithOnlyValidLiveCells() {
-        val universe = Universe(UNIVERS_SIZE)
+        val universe = Universe(3)
         assertAllCellsAreDead(universe)
         universe.startUniverse(Pair(-1, 1), Pair(2, 1))
-        assertThat(universe.cells[2][1].isAlive).isEqualTo(true)
+        assertThat(universe.cells[2][1].isAlive).isTrue()
+        universe.evolve()
     }
+
+    @Test
+    fun universeBlockShouldNotChange() {
+        val universe = Universe(2)
+        assertAllCellsAreDead(universe)
+        universe.startUniverse(Pair(0, 0), Pair(0, 1),
+                Pair(1, 0), Pair(1, 1))
+        assertThat(universe.cells[1][1].isAlive).isTrue()
+        universe.evolve()
+        assertThat(universe.cells[1][1].isAlive).isTrue()
+        universe.cells.forEach { row ->
+            row.forEach { cell ->
+                assertThat(cell.isAlive).isTrue()
+            }
+        }
+    }
+
+
+
+    @Test
+    fun test8x8Glider() {
+        val universe = Universe(8)
+        assertAllCellsAreDead(universe)
+
+        universe.startUniverse(Pair(3, 3), Pair(4, 4),
+                Pair(5, 2), Pair(5, 3), Pair(5,4))
+        assertThat(universe.cells[3][3].isAlive).isTrue()
+        assertThat(universe.cells[4][4].isAlive).isTrue()
+        assertThat(universe.cells[5][2].isAlive).isTrue()
+        assertThat(universe.cells[5][3].isAlive).isTrue()
+        assertThat(universe.cells[5][4].isAlive).isTrue()
+
+        universe.evolve()
+
+        assertThat(universe.cells[3][3].isAlive).isFalse()
+        assertThat(universe.cells[5][2].isAlive).isFalse()
+
+        assertThat(universe.cells[4][2].isAlive).isTrue()
+        assertThat(universe.cells[4][4].isAlive).isTrue()
+        assertThat(universe.cells[5][3].isAlive).isTrue()
+        assertThat(universe.cells[5][4].isAlive).isTrue()
+        assertThat(universe.cells[6][3].isAlive).isTrue()
+
+        universe.evolve()
+
+        assertThat(universe.cells[5][3].isAlive).isFalse()
+        assertThat(universe.cells[4][2].isAlive).isFalse()
+
+        assertThat(universe.cells[4][4].isAlive).isTrue()
+        assertThat(universe.cells[5][2].isAlive).isTrue()
+        assertThat(universe.cells[5][4].isAlive).isTrue()
+        assertThat(universe.cells[6][3].isAlive).isTrue()
+        assertThat(universe.cells[6][4].isAlive).isTrue()
+    }
+
+
 
 }
